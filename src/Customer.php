098dd88a764e62c2fb45d7eef68062b7a7e3d1d4
wahidin <wahidin_83@yahoo.com>
2024-04-21 11:21:51 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name my name is $this->name" . PHP_EOL;
+    }
+}
